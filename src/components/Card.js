3998f03b5c5c403a526ff29776f10fa6e837b7d8
James Park <james.hansung.park@gmail.com>
2022-03-24 14:54:31 -0400

    added icons with tailwind css

--- src/components/Card.js
+++ src/components/Card.js
@@ -13,6 +13,18 @@ const Card = ({ imgSrc, name, chef, cookTime }) => {
         <span className="block text-gray-500 text-sm">Recipe by {chef}</span>
       </div>
       <div className="badge">
+        <svg
+          class="w-6 h-6 inline-block"
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
         <span>
           {cookTime} {cookTime > 1 ? 'mins' : 'min'}
         </span>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,7 +5,7 @@ const Nav = () => {
     <div className="md:col-span-1">
       <nav>
         <div>
-          <h1 className="font-bold uppercase p-4 border-b border-gray-100"> 
+          <h1 className="font-bold uppercase p-4 border-b border-gray-100">
             <a href="/">Food Ninja</a>
           </h1>
         </div>
@@ -13,16 +13,56 @@ const Nav = () => {
           <li className="text-gray-700 font-bold">
             <a href="#">
               <span>Home</span>
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+                ></path>
+              </svg>
             </a>
           </li>
           <li>
             <a href="#">
               <span>About</span>
+              <svg
+                class="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
             </a>
           </li>
           <li>
             <a href="#">
               <span>Contact</span>
+              <svg
+                class="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                ></path>
+              </svg>
             </a>
           </li>
         </ul>
