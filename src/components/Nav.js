d4ffcaff594015effac41765179ab27f2ae53633
James Park <james.hansung.park@gmail.com>
2022-03-24 15:34:35 -0400

    added responsive formatting to navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,19 +2,19 @@ import React from 'react';
 
 const Nav = () => {
   return (
-    <div className="md:col-span-1">
-      <nav>
+    <div className="md:col-span-1 md:flex md:justify-end">
+      <nav className="text-right">
         <div>
           <h1 className="font-bold uppercase p-4 border-b border-gray-100">
             <a href="/" className="hover:text-gray-700">Food Ninja</a>
           </h1>
         </div>
-        <ul>
-          <li className="text-gray-700 font-bold">
-            <a href="#">
+        <ul className="text-sm mt-6">
+          <li className="text-gray-700 font-bold py-1">
+            <a href="#" className="px-4 flex justify-end border-r-4 border-red-400">
               <span>Home</span>
               <svg
-                class="w-6 h-6"
+                class="w-5 h-5 ml-2"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
@@ -29,11 +29,11 @@ const Nav = () => {
               </svg>
             </a>
           </li>
-          <li>
-            <a href="#">
+          <li className="py-1">
+            <a href="#" className="px-4 flex justify-end border-r-4 border-white">
               <span>About</span>
               <svg
-                class="w-6 h-6"
+                class="w-5 h-5 ml-2"
                 fill="currentColor"
                 viewBox="0 0 20 20"
                 xmlns="http://www.w3.org/2000/svg"
@@ -46,11 +46,11 @@ const Nav = () => {
               </svg>
             </a>
           </li>
-          <li>
-            <a href="#">
+          <li className="py-1">
+            <a href="#" className="px-4 flex justify-end border-r-4 border-white">
               <span>Contact</span>
               <svg
-                class="w-6 h-6"
+                class="w-5 h-5 ml-2"
                 fill="none"
                 stroke="currentColor"
                 viewBox="0 0 24 24"
